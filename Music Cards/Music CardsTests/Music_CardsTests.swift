fa973800b6650f610515dbfa387deaaa495f9b4f
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 21:51:39 -0400

    Add Music Cards/Music CardsTests/Music_CardsTests.swift

---
+++ Music Cards/Music CardsTests/Music_CardsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Music_CardsTests.swift
+//  Music CardsTests
+//
+//  Created by Elliot Williams on 2025-08-06.
+//
+
+import Testing
+@testable import Music_Cards
+
+struct Music_CardsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
